23187cb0485f69cd58e6432938c6b79899b19712
Spavatii <72168387+Spavatii@users.noreply.github.com>
2022-10-29 10:52:43 -0300

    Atualização : 1.01 = Estruturação Inicial do Programa Alexandria

--- Alexandria/Form1.Designer.cs
+++ Alexandria/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Alexandria
 {
-    partial class Form1
+    partial class FrmAlexandria
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAlexandria));
+            this.LabAlexandria = new System.Windows.Forms.Label();
+            this.LabMorse = new System.Windows.Forms.Label();
+            this.BtnObrasFisicas = new System.Windows.Forms.Button();
+            this.BtnObrasVisuais = new System.Windows.Forms.Button();
+            this.BtnAlexandria = new System.Windows.Forms.Button();
+            this.BtnFAQ = new System.Windows.Forms.Button();
+            this.BtnNotasAtt = new System.Windows.Forms.Button();
+            this.BtnFuturasAtt = new System.Windows.Forms.Button();
+            this.BtnSite = new System.Windows.Forms.Button();
+            this.BtnGitHub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabAlexandria
+            // 
+            this.LabAlexandria.AutoSize = true;
+            this.LabAlexandria.Location = new System.Drawing.Point(507, 68);
+            this.LabAlexandria.Name = "LabAlexandria";
+            this.LabAlexandria.Size = new System.Drawing.Size(56, 13);
+            this.LabAlexandria.TabIndex = 0;
+            this.LabAlexandria.Text = "Alexandria";
+            // 
+            // LabMorse
+            // 
+            this.LabMorse.AutoSize = true;
+            this.LabMorse.Location = new System.Drawing.Point(487, 126);
+            this.LabMorse.Name = "LabMorse";
+            this.LabMorse.Size = new System.Drawing.Size(115, 13);
+            this.LabMorse.TabIndex = 1;
+            this.LabMorse.Text = "- .-. ..- -- .. -. .. --.- ..- .. --";
+            // 
+            // BtnObrasFisicas
+            // 
+            this.BtnObrasFisicas.Location = new System.Drawing.Point(418, 184);
+            this.BtnObrasFisicas.Name = "BtnObrasFisicas";
+            this.BtnObrasFisicas.Size = new System.Drawing.Size(113, 23);
+            this.BtnObrasFisicas.TabIndex = 2;
+            this.BtnObrasFisicas.Text = "Obras Físicas";
+            this.BtnObrasFisicas.UseVisualStyleBackColor = true;
+            // 
+            // BtnObrasVisuais
+            // 
+            this.BtnObrasVisuais.Location = new System.Drawing.Point(418, 232);
+            this.BtnObrasVisuais.Name = "BtnObrasVisuais";
+            this.BtnObrasVisuais.Size = new System.Drawing.Size(113, 23);
+            this.BtnObrasVisuais.TabIndex = 3;
+            this.BtnObrasVisuais.Text = "Obras Visuais";
+            this.BtnObrasVisuais.UseVisualStyleBackColor = true;
+            // 
+            // BtnAlexandria
+            // 
+            this.BtnAlexandria.Location = new System.Drawing.Point(451, 332);
+            this.BtnAlexandria.Name = "BtnAlexandria";
+            this.BtnAlexandria.Size = new System.Drawing.Size(75, 23);
+            this.BtnAlexandria.TabIndex = 4;
+            this.BtnAlexandria.Text = "Alexandria";
+            this.BtnAlexandria.UseVisualStyleBackColor = true;
+            // 
+            // BtnFAQ
+            // 
+            this.BtnFAQ.Location = new System.Drawing.Point(451, 371);
+            this.BtnFAQ.Name = "BtnFAQ";
+            this.BtnFAQ.Size = new System.Drawing.Size(75, 23);
+            this.BtnFAQ.TabIndex = 5;
+            this.BtnFAQ.Text = "FAQ";
+            this.BtnFAQ.UseVisualStyleBackColor = true;
+            // 
+            // BtnNotasAtt
+            // 
+            this.BtnNotasAtt.Location = new System.Drawing.Point(641, 332);
+            this.BtnNotasAtt.Name = "BtnNotasAtt";
+            this.BtnNotasAtt.Size = new System.Drawing.Size(123, 23);
+            this.BtnNotasAtt.TabIndex = 6;
+            this.BtnNotasAtt.Text = "Notas de Atualização";
+            this.BtnNotasAtt.UseVisualStyleBackColor = true;
+            // 
+            // BtnFuturasAtt
+            // 
+            this.BtnFuturasAtt.Location = new System.Drawing.Point(641, 370);
+            this.BtnFuturasAtt.Name = "BtnFuturasAtt";
+            this.BtnFuturasAtt.Size = new System.Drawing.Size(123, 23);
+            this.BtnFuturasAtt.TabIndex = 7;
+            this.BtnFuturasAtt.Text = "Futuras Atualizações";
+            this.BtnFuturasAtt.UseVisualStyleBackColor = true;
+            // 
+            // BtnSite
+            // 
+            this.BtnSite.Location = new System.Drawing.Point(487, 445);
+            this.BtnSite.Name = "BtnSite";
+            this.BtnSite.Size = new System.Drawing.Size(75, 23);
+            this.BtnSite.TabIndex = 8;
+            this.BtnSite.Text = "Site";
+            this.BtnSite.UseVisualStyleBackColor = true;
+            // 
+            // BtnGitHub
+            // 
+            this.BtnGitHub.Location = new System.Drawing.Point(618, 444);
+            this.BtnGitHub.Name = "BtnGitHub";
+            this.BtnGitHub.Size = new System.Drawing.Size(75, 23);
+            this.BtnGitHub.TabIndex = 9;
+            this.BtnGitHub.Text = "GitHub";
+            this.BtnGitHub.UseVisualStyleBackColor = true;
+            // 
+            // FrmAlexandria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1104, 583);
+            this.Controls.Add(this.BtnGitHub);
+            this.Controls.Add(this.BtnSite);
+            this.Controls.Add(this.BtnFuturasAtt);
+            this.Controls.Add(this.BtnNotasAtt);
+            this.Controls.Add(this.BtnFAQ);
+            this.Controls.Add(this.BtnAlexandria);
+            this.Controls.Add(this.BtnObrasVisuais);
+            this.Controls.Add(this.BtnObrasFisicas);
+            this.Controls.Add(this.LabMorse);
+            this.Controls.Add(this.LabAlexandria);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmAlexandria";
+            this.Text = "Alexandria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LabAlexandria;
+        private System.Windows.Forms.Label LabMorse;
+        private System.Windows.Forms.Button BtnObrasFisicas;
+        private System.Windows.Forms.Button BtnObrasVisuais;
+        private System.Windows.Forms.Button BtnAlexandria;
+        private System.Windows.Forms.Button BtnFAQ;
+        private System.Windows.Forms.Button BtnNotasAtt;
+        private System.Windows.Forms.Button BtnFuturasAtt;
+        private System.Windows.Forms.Button BtnSite;
+        private System.Windows.Forms.Button BtnGitHub;
     }
 }
 
--- Alexandria/Form1.cs
+++ Alexandria/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Alexandria
 {
-    public partial class Form1 : Form
+    public partial class FrmAlexandria : Form
     {
-        public Form1()
+        public FrmAlexandria()
         {
             InitializeComponent();
         }
